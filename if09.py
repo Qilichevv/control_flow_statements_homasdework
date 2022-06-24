6da0218fc23bd7658d68adf79ee8d8e581fd8495
qilichevv <behruzqilichevv23@gmail.com>
2022-06-24 14:59:57 +0300

    did

--- if09.py
+++ if09.py
@@ -9,4 +9,9 @@ def main(a):
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False.
     """
-    return
+    a1=a%10
+    a=a//10
+    a2=a%10
+    s=a1*10+a2
+    return a>=s
+print(main(22))
